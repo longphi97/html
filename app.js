48ce2f5e6ff2f93a4003263e94264a65c0a5f2a6
LongPhiLong <gangsteridol977@gmail.com>
2022-04-21 08:54:28 +0700

    beta

---
+++ app.js
@@ -0,0 +1,13 @@
+function App(props) {
+    return (
+        <Pewpew>
+            <div>
+                <h1>
+                    are u ok?
+                </h1>
+            </div>
+        </Pewpew>
+    )
+}
+
+export default App;
